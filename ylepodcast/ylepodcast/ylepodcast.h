d5f89c347d437a4d752a9514a32acf376486af5e
Arto Heino <artohei@artos-macbook-pro.local>
2016-10-27 09:42:55 +0300

    New project added

---
+++ ylepodcast/ylepodcast/ylepodcast.h
@@ -0,0 +1,19 @@
+//
+//  ylepodcast.h
+//  ylepodcast
+//
+//  Created by Arto Heino on 27/10/16.
+//  Copyright © 2016 Metropolia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ylepodcast.
+FOUNDATION_EXPORT double ylepodcastVersionNumber;
+
+//! Project version string for ylepodcast.
+FOUNDATION_EXPORT const unsigned char ylepodcastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ylepodcast/PublicHeader.h>
+
+
