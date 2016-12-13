630bcc5fa80ebd212affc5c3ea270093bedf8168
Arto Heino <artohei@Artos-MacBook-Pro.local>
2016-12-14 01:40:37 +0200

    Test added

--- ylepodcast/ylepodcastTests/ylepodcastTests.swift
+++ ylepodcast/ylepodcastTests/ylepodcastTests.swift
@@ -14,32 +14,6 @@ class ylepodcastTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        func testSearchWithValidSearchString() {
-            DatabaseController.clear(table: "Podcast")
-            
-            let context = DatabaseController.getContext()
-            
-            // Add one podcast to coredata with title "Kissa"
-            let podcast1 = Podcast(context: context)
-            podcast1.podcastTitle = "Kissa"
-            
-            // Add one podcast to coredata with title "Koira"
-            let podcast2 = Podcast(context: context)
-            podcast2.podcastTitle = "Koira"
-            
-            DatabaseController.saveContext()
-            
-            // Create SearchTableViewController
-            let searchViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Search") as! SearchTableViewController
-            searchViewController.loadViewIfNeeded()
-            
-            // Scope 0 searches for title with search string "kissa"
-            searchViewController.getData(scope: 0, searchString: "kissa")
-            
-            // Make sure we have one podcast visible in search results
-            XCTAssertTrue(searchViewController.tableView.visibleCells.count == 1)
-        }
-        
     }
     
     override func tearDown() {
@@ -47,9 +21,30 @@ class ylepodcastTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSearchWithValidSearchString() {
+        DatabaseController.clear(table: "Podcast")
+        
+        let context = DatabaseController.getContext()
+        
+        // Add one podcast to coredata with title "Kissa"
+        let podcast1 = Podcast(context: context)
+        podcast1.podcastTitle = "Kissa"
+        
+        // Add one podcast to coredata with title "Koira"
+        let podcast2 = Podcast(context: context)
+        podcast2.podcastTitle = "Koira"
+        
+        DatabaseController.saveContext()
+        
+        // Create SearchTableViewController
+        let searchViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SearchTableViewController") as! SearchTableViewController
+        searchViewController.loadViewIfNeeded()
+        
+        // Scope 0 searches for title with search string "kissa"
+        searchViewController.getData(scope: 0, searchString: "kissa")
+        
+        // Make sure we have one podcast visible in search results
+        XCTAssertTrue(searchViewController.tableView.visibleCells.count == 1)
     }
     
     func testPerformanceExample() {
